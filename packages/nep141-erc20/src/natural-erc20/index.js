a0883acca8e51335e2805f6e51cec5d12f60babd
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-03-11 22:46:16 +0900

    feat: add eth->near transfer recovery from lock hash

--- packages/nep141-erc20/src/natural-erc20/index.js
+++ packages/nep141-erc20/src/natural-erc20/index.js
@@ -1,3 +1,4 @@
 export { default as getMetadata } from './getMetadata'
 export { default as getName } from './getName'
 export { initiate as sendToNear } from './sendToNear'
+export { recover } from './sendToNear'
