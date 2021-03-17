1c3469615bca6312a4fb926965f8db8922d8fa47
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-03-17 16:42:25 +0900

    fix: prevent fail when no tx hash before redirect

--- packages/nep141-erc20/src/bridged-nep141/sendToEthereum/index.js
+++ packages/nep141-erc20/src/bridged-nep141/sendToEthereum/index.js
@@ -217,16 +217,11 @@ export async function checkWithdraw (transfer) {
   }
   if (!txHash) {
     // If checkWithdraw is called before withdraw sig wallet redirect
-    // record the error but don't mark as FAILED and don't clear url params
+    // log the error but don't mark as FAILED and don't clear url params
     // as the wallet redirect has not happened yet
-    urlParams.clear()
-    const newError = 'Error from wallet: txHash not received'
-    console.error(newError)
-    return {
-      ...transfer,
-      status: status.FAILED,
-      errors: [...transfer.errors, newError]
-    }
+    const newError = 'Withdraw tx hash not received: pending redirect or wallet error'
+    console.log(newError)
+    return transfer
   }
   if (txHash.includes(',')) {
     urlParams.clear()
