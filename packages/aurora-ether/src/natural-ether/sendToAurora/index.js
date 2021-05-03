c1309b87bd8c4b2bcc4a80c6e875d679bb4e962e
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-05-03 18:15:49 +0900

    feat(wip): Add aurora-ether sendToAurora

--- packages/aurora-ether/src/natural-ether/sendToAurora/index.js
+++ packages/aurora-ether/src/natural-ether/sendToAurora/index.js
@@ -8,14 +8,12 @@ import { findReplacementTx } from '../../utils'
 
 export const SOURCE_NETWORK = 'ethereum'
 export const DESTINATION_NETWORK = 'aurora'
-export const TRANSFER_TYPE = '@near-eth/aurora-erc20/natural-erc20/sendToAurora'
+export const TRANSFER_TYPE = '@near-eth/aurora-ether/natural-ether/sendToAurora'
 
-const APPROVE = 'approve-natural-erc20-to-aurora'
-const LOCK = 'lock-natural-erc20-to-aurora'
-const SYNC = 'sync-natural-erc20-to-aurora'
+const LOCK = 'lock-natural-ether-to-aurora'
+const SYNC = 'sync-natural-ether-to-aurora'
 
 const steps = [
-  APPROVE,
   LOCK,
   SYNC
 ]
@@ -41,8 +39,6 @@ const transferDraft = {
   // }
 
   // Attributes specific to natural-erc20-to-nep141 transfers
-  approvalHashes: [],
-  approvalReceipts: [],
   completedConfirmations: 0,
   lockHashes: [],
   lockReceipts: [],
@@ -53,7 +49,6 @@ const transferDraft = {
 export const i18n = {
   en_US: {
     steps: transfer => stepsFor(transfer, steps, {
-      [APPROVE]: `Approve transfer of ${formatLargeNum(transfer.amount, transfer.decimals)} ${transfer.sourceTokenName} from Ethereum`,
       [LOCK]: `Start transfer of ${formatLargeNum(transfer.amount, transfer.decimals)} ${transfer.sourceTokenName} to Aurora`,
       [SYNC]: `Wait for ${transfer.neededConfirmations} transfer confirmations for security`
     }),
@@ -61,14 +56,12 @@ export const i18n = {
       if (transfer.status === status.FAILED) return 'Failed'
       if (transfer.status === status.ACTION_NEEDED) {
         switch (transfer.completedStep) {
-          case APPROVE: return 'Ready to transfer from Ethereum'
-          case SYNC: return 'Ready to deposit in Aurora'
+          case SYNC: return 'Ready to deposit in NEAR'
           default: throw new Error(`Transfer in unexpected state, transfer with ID=${transfer.id} & status=${transfer.status} has completedStep=${transfer.completedStep}`)
         }
       }
       switch (transfer.completedStep) {
         case null: return 'Approving transfer'
-        case APPROVE: return 'Transfering to Aurora'
         case LOCK: return `Confirming transfer ${transfer.completedConfirmations + 1} of ${transfer.neededConfirmations}`
         case SYNC: return 'Depositing in Aurora'
         default: throw new Error(`Transfer in unexpected state, transfer with ID=${transfer.id} & status=${transfer.status} has completedStep=${transfer.completedStep}`)
@@ -78,7 +71,6 @@ export const i18n = {
       if (transfer.status === status.FAILED) return 'Retry'
       if (transfer.status !== status.ACTION_NEEDED) return null
       switch (transfer.completedStep) {
-        case APPROVE: return 'Transfer'
         case SYNC: return 'Deposit'
         default: return null
       }
@@ -92,8 +84,7 @@ export const i18n = {
  */
 export function act (transfer) {
   switch (transfer.completedStep) {
-    case null: return approve(transfer)
-    case APPROVE: return lock(transfer)
+    case null: return lock(transfer)
     case LOCK: return checkSync(transfer)
     default: throw new Error(`Don't know how to act on transfer: ${transfer.id}`)
   }
@@ -105,8 +96,7 @@ export function act (transfer) {
  */
 export function checkStatus (transfer) {
   switch (transfer.completedStep) {
-    case null: return checkApprove(transfer)
-    case APPROVE: return checkLock(transfer)
+    case null: return checkLock(transfer)
     case LOCK: return checkSync(transfer)
     default: throw new Error(`Don't know how to checkStatus for transfer ${transfer.id}`)
   }
@@ -148,171 +138,11 @@ export async function initiate ({ amount, token }) {
 
   transfer = await lock({
     ...transfer,
-    completedStep: APPROVE,
     status: status.ACTION_NEEDED
   })
 
   track(transfer)
   return transfer
-}
-
-export async function approve ({ amount, token }) {
-  const sourceTokenName = token.symbol
-  // TODO: call initiate with a formated amount and query decimals when decorate()
-  const decimals = token.decimals
-  const destinationTokenName = 'a' + sourceTokenName
-
-  const web3 = new Web3(getSignerProvider())
-
-  const ethNetwork = await web3.eth.net.getNetworkType()
-  if (ethNetwork !== process.env.ethNetworkId) {
-    // Webapp should prevent the user from confirming if the wrong network is selected
-    throw new Error(
-      'Wrong eth network for checkLock, expected: %s, got: %s',
-      process.env.ethNetworkId, ethNetwork
-    )
-  }
-
-  // TODO enable different recipient and consider multisig case where sender is not the signer
-  const sender = web3.currentProvider.selectedAddress
-  const recipient = sender
-
-  // various attributes stored as arrays, to keep history of retries
-  const transfer = {
-    ...transferDraft,
-
-    amount: amount.toString(),
-    destinationTokenName,
-    recipient,
-    sender,
-    sourceToken: token.address,
-    sourceTokenName,
-    decimals
-  }
-
-  const erc20Contract = new web3.eth.Contract(
-    JSON.parse(process.env.ethErc20AbiText),
-    token.address,
-    { from: sender }
-  )
-
-  // If this tx is dropped and replaced, lower the search boundary
-  // in case there was a reorg.
-  // const safeReorgHeight = await web3.eth.getBlockNumber() - 20
-  const approvalHash = await new Promise((resolve, reject) => {
-    erc20Contract.methods
-      .approve(process.env.ethLockerAddress, amount).send()
-      .on('transactionHash', resolve)
-      .catch(reject)
-  })
-  // const pendingApprovalTx = await web3.eth.getTransaction(approvalHash)
-
-  return {
-    ...transfer,
-    // ethCache: {
-    //   from: pendingApprovalTx.from,
-    //   safeReorgHeight,
-    //   nonce: pendingApprovalTx.nonce
-    // },
-    approvalHashes: [...transfer.approvalHashes, approvalHash],
-    status: status.IN_PROGRESS
-  }
-}
-
-async function checkApprove ({ amount, token }) {
-// export async function getAllowance ({ sender, token }) {
-  /*
-  A transfer is recorded after the lock step. So the approval hash is not recorded in
-  a transfer object in local storage.
-  Instead of checking that transaction, we can instead check that the allowance has
-  become enough.
-  */
-  // TODO check allowance is larger than transfer amount.
-  /*
-  const web3 = new Web3(getEthProvider())
-
-  const erc20Contract = new web3.eth.Contract(
-    JSON.parse(process.env.ethErc20AbiText),
-    token.address
-  )
-
-  const allowance = await erc20Contract.methods.allowance(sender, process.env.ethLockerAddress).call()
-  console.log('allowance: ', allowance)
-  return allowance
-  const provider = getEthProvider()
-  // If available connect to rpcUrl to avoid issues with WalletConnectProvider
-  const web3 = new Web3(provider.rpcUrl ? provider.rpcUrl : provider)
-
-  const ethNetwork = await web3.eth.net.getNetworkType()
-  if (ethNetwork !== process.env.ethNetworkId) {
-    console.log(
-      'Wrong eth network for checkApprove, expected: %s, got: %s',
-      process.env.ethNetworkId, ethNetwork
-    )
-    return transfer
-  }
-
-  const approvalHash = last(transfer.approvalHashes)
-  let approvalReceipt = await web3.eth.getTransactionReceipt(approvalHash)
-
-  // If no receipt, check that the transaction hasn't been replaced (speedup or canceled)
-  if (!approvalReceipt) {
-    // don't break old transfers in case they were made before this functionality is released
-    if (!transfer.ethCache) return transfer
-    try {
-      const tx = {
-        nonce: transfer.ethCache.nonce,
-        from: transfer.ethCache.from,
-        to: transfer.sourceToken
-      }
-      const event = {
-        name: 'Approval',
-        abi: process.env.ethErc20AbiText,
-        validate: ({ returnValues: { owner, spender, value } }) => {
-          return (
-            owner.toLowerCase() === transfer.sender.toLowerCase() &&
-            spender.toLowerCase() === process.env.ethLockerAddress.toLowerCase()
-            // Don't check value as the user may have increased approval before signing.
-            // value === transfer.amount
-          )
-        }
-      }
-      approvalReceipt = await findReplacementTx(transfer.ethCache.safeReorgHeight, tx, event)
-    } catch (error) {
-      console.error(error)
-      return {
-        ...transfer,
-        errors: [...transfer.errors, error.message],
-        status: status.FAILED
-      }
-    }
-  }
-  if (!approvalReceipt) return transfer
-
-  if (!approvalReceipt.status) {
-    let error
-    try {
-      error = await getRevertReason(approvalHash, ethNetwork)
-    } catch (e) {
-      console.error(e)
-      error = `Could not determine why transaction '${approvalReceipt.transactionHash}'
-        failed; encountered error: ${e.message}`
-    }
-    return {
-      ...transfer,
-      approvalReceipts: [...transfer.approvalReceipts, approvalReceipt],
-      errors: [...transfer.errors, error],
-      status: status.FAILED
-    }
-  }
-
-  return {
-    ...transfer,
-    approvalReceipts: [...transfer.approvalReceipts, approvalReceipt],
-    completedStep: APPROVE,
-    status: status.ACTION_NEEDED
-  }
-  */
 }
 
 /**
@@ -335,8 +165,8 @@ async function lock (transfer) {
   }
 
   const ethTokenLocker = new web3.eth.Contract(
-    JSON.parse(process.env.ethLockerAbiText),
-    process.env.ethLockerAddress,
+    JSON.parse(process.env.etherCustodianAbiText),
+    process.env.etherCustodianAddress,
     { from: transfer.sender }
   )
 
@@ -345,12 +175,11 @@ async function lock (transfer) {
   const safeReorgHeight = await web3.eth.getBlockNumber() - 20
   const lockHash = await new Promise((resolve, reject) => {
     ethTokenLocker.methods
-      .lockToken(
-        transfer.sourceToken,
-        transfer.amount,
-        'TODO evm_address' + ':' + transfer.recipient
+      .depositToEVM(
+        transfer.recipient,
+        '1' // TODO uint256 fee,
       )
-      .send()
+      .send({ value: transfer.amount })
       .on('transactionHash', resolve)
       .catch(reject)
   })
@@ -395,15 +224,15 @@ async function checkLock (transfer) {
         to: process.env.ethLockerAddress
       }
       const event = {
-        name: 'Locked',
-        abi: process.env.ethLockerAbiText,
-        validate: ({ returnValues: { token, sender, amount, accountId } }) => {
+        name: 'Deposited',
+        abi: process.env.etherCustodianAbiText,
+        validate: ({ returnValues: { sender, recipient, amount, fee } }) => {
           if (!event) return false
           return (
-            token.toLowerCase() === transfer.sourceToken.toLowerCase() &&
             sender.toLowerCase() === transfer.sender.toLowerCase() &&
+            recipient.toLowerCase() === transfer.recipient.toLowerCase() &&
             amount === transfer.amount &&
-            accountId === 'TODO evm_address' + ':' + transfer.recipient // TODO account id is aurora_near_account:recipient_eth_address
+            fee === '1' // TODO
           )
         }
       }
