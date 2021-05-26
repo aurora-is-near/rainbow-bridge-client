33f4c5b4adf6e15277d9a7ee30793d52a3ad8290
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2021-05-26 19:28:42 +0900

    fix: Handle network errors for findReplacementTx.
    Check for SearchError to differentiate from of types of errors like
    networking.

--- packages/near-ether/src/bridged-near/sendToNear/index.js
+++ packages/near-ether/src/bridged-near/sendToNear/index.js
@@ -9,7 +9,7 @@ import { stepsFor } from '@near-eth/client/dist/i18nHelpers'
 import * as status from '@near-eth/client/dist/statuses'
 import { getEthProvider, getNearAccount, formatLargeNum } from '@near-eth/client/dist/utils'
 import { urlParams, ethOnNearSyncHeight } from '@near-eth/utils'
-import { findReplacementTx } from 'find-replacement-tx'
+import { findReplacementTx, SearchError } from 'find-replacement-tx'
 import findProof from './findProof'
 
 export const SOURCE_NETWORK = 'ethereum'
@@ -279,11 +279,14 @@ async function checkBurn (transfer) {
       burnReceipt = await web3.eth.getTransactionReceipt(foundTx.hash)
     } catch (error) {
       console.error(error)
-      return {
-        ...transfer,
-        errors: [...transfer.errors, error.message],
-        status: status.FAILED
+      if (error instanceof SearchError) {
+        return {
+          ...transfer,
+          errors: [...transfer.errors, error.message],
+          status: status.FAILED
+        }
       }
+      throw error
     }
   }
 
