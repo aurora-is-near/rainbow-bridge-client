a30c6b2acf6773a9fd529803b2d0ec30556cff5d
Pierre-Alain <pierre-alain.ouvrard@aurora.dev>
2021-07-08 15:04:14 +0900

    feat: Add ETH connector to `@near-eth/near-ether`.

---
+++ packages/near-ether/src/bridged-ether/index.js
@@ -0,0 +1,2 @@
+export { initiate as sendToEthereum } from './sendToEthereum'
+export { recover } from './sendToEthereum'
--- packages/near-ether/src/index.js
+++ packages/near-ether/src/index.js
@@ -1,2 +1,4 @@
 export * as naturalNEAR from './natural-near'
 export * as bridgedNEAR from './bridged-near'
+export * as naturalETH from './natural-ether'
+export * as bridgedETH from './bridged-ether'
