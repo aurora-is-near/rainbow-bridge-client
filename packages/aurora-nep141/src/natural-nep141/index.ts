8cc2ce934ea500cf600e1d66d30588315ff639c0
Pierre-Alain <pierre-alain.ouvrard@aurora.dev>
2021-09-16 11:55:30 +0900

    feat: Add findAllTransfers for NEAR -> Aurora.

--- packages/aurora-nep141/src/natural-nep141/index.ts
+++ packages/aurora-nep141/src/natural-nep141/index.ts
@@ -1,3 +1,4 @@
 export { sendToAurora, wrapAndSendNearToAurora } from './sendToAurora'
 export { default as getMetadata } from './getMetadata'
 export { default as getBalance } from './getBalance'
+export { findAllTransfers } from './sendToAurora'
