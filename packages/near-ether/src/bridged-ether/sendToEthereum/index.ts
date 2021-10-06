dc6937afff39384622c64cc30aaa011a40b16dff
Pierre-Alain <pierre-alain.ouvrard@aurora.dev>
2021-10-06 21:11:07 +0900

    style: Update error msg for uncertain NEAR wallet redirect.

--- packages/near-ether/src/bridged-ether/sendToEthereum/index.ts
+++ packages/near-ether/src/bridged-ether/sendToEthereum/index.ts
@@ -579,8 +579,8 @@ export async function checkBurn (
     // This doesn't protect agains the user broadcasting a tx and closing the tab before
     // redirect. So the dapp has no way of knowing the status of that transaction.
     // Set status to FAILED so that it can be retried
-    const newError = `A withdraw transaction was initiated but could not be verified.
-      Click 'rescan the blockchain' to check if a transfer was made.`
+    const newError = `A transaction was initiated but could not be verified.
+      Click 'Rescan the blockchain' to check if a transfer was created.`
     console.error(newError)
     return {
       ...transfer,
--- packages/near-ether/src/natural-near/sendToEthereum/index.ts
+++ packages/near-ether/src/natural-near/sendToEthereum/index.ts
@@ -560,8 +560,8 @@ export async function checkLock (
     // This doesn't protect agains the user broadcasting a tx and closing the tab before
     // redirect. So the dapp has no way of knowing the status of that transaction.
     // Set status to FAILED so that it can be retried
-    const newError = `A lock transaction was initiated but could not be verified.
-      Click 'rescan the blockchain' to check if a transfer was made.`
+    const newError = `A transaction was initiated but could not be verified.
+      Click 'Rescan the blockchain' to check if a transfer was created.`
     console.error(newError)
     return {
       ...transfer,
