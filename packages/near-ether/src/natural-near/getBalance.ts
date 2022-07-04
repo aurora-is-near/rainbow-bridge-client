364099b3e9bfa6939f9f673c86035002da7a46e8
Pierre-Alain Ouvrard <pierre-alain.ouvrard@aurora.dev>
2022-07-04 16:18:19 +0900

    refactor: Get NEAR balance with naj provider.

--- packages/near-ether/src/natural-near/getBalance.ts
+++ packages/near-ether/src/natural-near/getBalance.ts
@@ -1,7 +1,34 @@
-import { getNearAccount } from '@near-eth/client/dist/utils'
+import BN from 'bn.js'
+import { Account, providers as najProviders } from 'near-api-js'
+import { AccountView } from 'near-api-js/lib/providers/provider'
+import { getNearProvider } from '@near-eth/client/dist/utils'
 
-export default async function getBalance (): Promise<string> {
-  const nearAccount = await getNearAccount()
-  const { available: nearBalance } = await nearAccount.getAccountBalance()
-  return nearBalance
+export default async function getBalance (
+  { owner, options }: {
+    owner: string
+    options?: {
+      nearAccount?: Account
+      nearProvider?: najProviders.Provider
+    }
+  }
+): Promise<string> {
+  options = options ?? {}
+  const nearProvider =
+    options.nearProvider ??
+    options.nearAccount?.connection.provider ??
+    getNearProvider()
+
+  // https://github.com/near/near-api-js/blob/fae90841ee320d812c352e2116cf974dbc283fc7/src/account.ts#L645
+  const protocolConfig = await nearProvider.experimental_protocolConfig({ finality: 'final' })
+  const state = await nearProvider.query<AccountView>({
+    request_type: 'view_account',
+    account_id: owner,
+    finality: 'optimistic'
+  })
+  const costPerByte = new BN(protocolConfig.runtime_config.storage_amount_per_byte)
+  const stateStaked = new BN(state.storage_usage).mul(costPerByte)
+  const staked = new BN(state.locked)
+  const totalBalance = new BN(state.amount).add(staked)
+  const availableBalance = totalBalance.sub(BN.max(staked, stateStaked)).toString()
+  return availableBalance
 }
